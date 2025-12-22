74d9f7d9991182485a2e4857d68712745d73a672
Teshome-worku <teshomeworku96@gmail.com>
2025-12-22 20:10:57 +0300

    feat: add validation and error handling for note title and content

--- src/pages/AddNote.jsx
+++ src/pages/AddNote.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SavePopup from "../components/SavePopup";
 const AddNote=()=>{
+    const titleRef=useRef(null);
+    const contentRef=useRef(null);
+
     const [title,setTitle]=useState("");
     const[content,setContent]=useState("");
+    
     const [date,setDate]=useState("");
     const [savePopup,setSavePopup]=useState(false);
     const navigate=useNavigate();
+    const [inCorrectTitle,setIncorrectTitle]=useState("");
+    const [inCorrectContent,setIncorrectContent]=useState("");
+
 
     const addNote=(e)=>{
         e.preventDefault();
+        if(!validate()) return;
         const arr=JSON.parse(localStorage.getItem("notes") || '[]');
         const obj={
             id:crypto.randomUUID(),
@@ -21,49 +29,99 @@ const AddNote=()=>{
         arr.push(obj);
         localStorage.setItem("notes",JSON.stringify(arr));
         setSavePopup(true)
+       
+    }
+    const validate=()=>{
+        if(!title.trim()){
+            setIncorrectTitle("Title cannot be empty.");
+            titleRef.current.focus();
+            setTimeout(()=>{
+                setIncorrectTitle("");
+            },3000)
+            return false
+        }
+        else if(title.trim().length<3 || title.trim().length>20){
+            setIncorrectTitle("Title must be between 3 and 20 characters.");
+            titleRef.current.focus();
+            setTimeout(()=>{
+                setIncorrectTitle("");
+            },3000)
+            return false;
+        }
+        else if(!content.trim()){
+            setIncorrectContent("Content cannot be empty.");
+            contentRef.current.focus();
+            setTimeout(()=>{
+                setIncorrectContent("");
+            },3000)
+            return false;
+        }
+        else if(content.trim().length<5 || content.trim().length>500){
+            setIncorrectContent("Content must be between 5 and 500 characters.");
+            contentRef.current.focus();
+            setTimeout(()=>{
+                setIncorrectContent("");
+            },3000)
+            return false;
+        }
+        else if(!date){
+            setIncorrectContent("Please select a date.");
+            dateRef.current.focus();    
+            setTimeout(()=>{
+                setIncorrectContent("");
+            },3000)
+            return false;
+        }
+       
+        else{
+            setIncorrectTitle("");
+            setIncorrectContent("");
+            return true;
+        }
+       
     }
     const closePopup=()=>{
         setSavePopup(false)
         navigate("/");
     }
-    const keyPressed=(e)=>{
-        if(e.key==="Enter"){
-            addNote();
-        }
-    }
+    
     return(
         <div className="add-note">
             
             <h1>Add New Note</h1>
             <div className="input-field">
-                <form onSubmit={addNote} onKeyDown={keyPressed}>
+                <form onSubmit={addNote} >
                     <p>Title</p>
                     <input type="text" 
-                    value={title}
+                    value={title.trimStart()}
+                    ref={titleRef}
                     required
                     placeholder="type your note title here..."
-                    minLength={3}
-                    maxLength={20}
+                   
                     onChange={(e)=>setTitle(e.target.value)}
                     />
+                    {inCorrectTitle && <span className="error">{inCorrectTitle}</span>}
                     <p>Content</p>
                     <textarea
-                    value={content}
+                    value={content.trimStart()}
+                    ref={contentRef}
                     placeholder="write your note content here..."
                     rows={5}
                     cols={50}
                     required
                     onChange={(e)=>setContent(e.target.value)}
                     />
+                    {inCorrectContent && <span className="error">{inCorrectContent}</span>}
                     <p>Date</p>
                     <input type="date" 
                     required
                     value={date}
                     onChange={(e)=>setDate(e.target.value)}
                     />
-                    <button type="submit">Save</button>
+                    <button type="submit" disabled={!title.trim() || !content.trim() } 
+                    style={{opacity: !title.trim() || !content.trim() || !date ? 0.6 : 1} }
+                    >Save</button>
                 </form>
-
             </div>
             {savePopup&&<SavePopup onClose={closePopup}/>}
 
